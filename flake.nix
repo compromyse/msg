9bdcb9c91f03db4dc9dcee232927f895f4083fcb
Raghuram Subramani <raghus2247@gmail.com>
2024-11-07 15:42:20 +0530

    nix-flake: Update & direnv

--- flake.nix
+++ flake.nix
@@ -5,15 +5,15 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in {
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          clang-tools_18
           clang_18
-          lldb
           gnumake
 
+          clang-tools_18
+          lldb
           bear
         ];
       };
