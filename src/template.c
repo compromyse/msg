6d3a1f6a1f3a92b726e55ced046c10d2936ee54f
Raghuram Subramani <raghus2247@gmail.com>
2025-07-10 21:52:42 +0530

    template: use lex() to create template->components

--- src/template.c
+++ src/template.c
@@ -1,10 +1,7 @@
 #define _GNU_SOURCE
 
-#include <engine.h>
 #include <filehandler.h>
 #include <lexer.h>
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <template.h>
 
@@ -14,7 +11,6 @@ template_t *
 template_create(void)
 {
   template_t *template = malloc(sizeof(template_t));
-  template->components = list_create(sizeof(directive_t));
 
   FILE *base = fopen(DIRECTORY "/" BASE_TEMPLATE, "r");
 
@@ -22,39 +18,7 @@ template_create(void)
   char *buffer = fcontent(base, size);
   fclose(base);
 
-  key_match_t *match;
-  while (true) {
-    match = find_next_key(buffer);
-    if (match == NULL)
-      break;
-
-    directive_t *directive = find_directive(buffer, match);
-    if (directive == NULL) {
-      printf(
-          "Unknown directive: %.*s\n", match->length, buffer + match->offset);
-
-      break;
-    }
-
-    switch (directive->type) {
-    case CONTENT:
-    case BODY:
-      break;
-
-    /* TODO: Handle this gracefully */
-    case INCLUDE:
-    case CONTENTFOR:
-    case ENDCONTENT:
-    case _RAW:
-      break;
-    }
-
-    if (directive != NULL)
-      free(directive);
-
-    if (match != NULL)
-      free(match);
-  }
+  template->components = lex(buffer);
 
   return template;
 }
