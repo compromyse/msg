f62c7ced4f529a069530e6effc370371338eb3af
Raghuram Subramani <raghus2247@gmail.com>
2025-07-11 12:35:34 +0530

    main: handle markdown files

--- src/main.c
+++ src/main.c
@@ -71,7 +71,7 @@ handle_file(const char *path)
 
   if (dot && strcmp(dot, ".md") == 0) {
     MMIOT *doc = mkd_string(buffer, size, 0);
-    template_write(base_template, NULL, out, doc, false);
+    template_write(base_template, NULL, out, doc, true);
   } else if (strlen(buffer) != 0) {
     list_t *content_headers = ingest(&buffer);
     template_write(base_template, content_headers, out, buffer, false);
@@ -118,15 +118,14 @@ main(int argc, char **argv)
     printf("HANDLING: %s\n", filepath);
     handle_file(filepath);
     free(filepath);
-
-    return EXIT_SUCCESS;
   }
 
-  /* for (x = (char **) md_resources; *x != NULL; x++) { */
-  /*   asprintf(&filepath, "%s.md", *x); */
-  /*   handle_file(filepath); */
-  /*   free(filepath); */
-  /* } */
+  for (x = (char **) md_resources; *x != NULL; x++) {
+    asprintf(&filepath, "%s.md", *x);
+    printf("HANDLING: %s\n", filepath);
+    handle_file(filepath);
+    free(filepath);
+  }
 
   return EXIT_SUCCESS;
 }
