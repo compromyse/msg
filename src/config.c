c514b27d48acc80f93defb540018bf2c4e00bf20
Raghuram Subramani <raghus2247@gmail.com>
2025-07-26 13:27:27 +0530

    config: use a list to keep track of keys and values

--- src/config.c
+++ src/config.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 
 #include <config.h>
+#include <list.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,19 +10,30 @@
 void
 config_parse(char *content)
 {
+  list_t *keys = list_create(sizeof(char *));
+  list_t *values = list_create(sizeof(char *));
+
   char *buffer = strdup(content);
 
   char *line_internal = strtok(buffer, "\n");
   while (line_internal != NULL) {
     char *line = strdup(line_internal);
-    char *key = strsep(&line, "=");
-    line = trim(line);
+
+    char *value = line;
+    char *key = strsep(&value, "=");
+    value = trim(value);
     key = trim(key);
 
-    printf("Key: %s\n", key);
-    printf("Value: %s\n", line);
+    list_add(keys, strdup(key));
+    list_add(values, strdup(value));
 
+    free(line);
     line_internal = strtok(NULL, "\n");
+  }
+
+  for (size_t i = 0; i < keys->size; i++) {
+    printf(
+        "%s: %s\n", (char *) list_get(keys, i), (char *) list_get(values, i));
   }
 
   free(buffer);
