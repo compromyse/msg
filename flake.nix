b3a125624d79432d74f6aba09429af92565d0def
Raghuram Subramani <raghus2247@gmail.com>
2025-08-01 11:06:58 +0530

    flake: no need to allow unfree packages

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
